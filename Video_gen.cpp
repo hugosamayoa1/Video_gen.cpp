08c9ea2201c4dc96e937ba2c751b6636c2e1da1f
hugosamayoa1 <32474143+hugosamayoa1@users.noreply.github.com>
2017-11-27 13:18:57 -0800

    Update Video_gen.cpp

--- Video_gen.cpp
+++ Video_gen.cpp
@@ -5,76 +5,79 @@
 #include <cassert>
 #include <cstring>
 
-typedef unsigned char byte;
 
-using namespace std;
 
 #define W 720
 #define H 480
 
-const double frames_per_second = 50;
-const int duration_per_second = 6;
+unsigned char frame[H][W][3];
+const double frame_per_seconds = 30;
+const int duration_per_seconds = 3;
 
-unsigned frame[H][W][6];
+typedef unsigned char byte;
 
-void black_screen(){ 
-	memset(frame, 0 ,sizeof(frame));
-	}
+class Video {
 	
-void rect_figure(int x, int y, int h, int w, byte r, byte g, byte b);
-
-// draws the black frame where rectangle will move on
-void frame_drawn(double t){
-
-	black_screen();
-	const double pps = 100;
-	rect_figure(150 + t * pps, 150 + t * pps, 50, 100, 0xff, 0xff, 0xff);
-}
-
-void clamp(int * x, int * y) {
-
-	if (*x < 0) 
-		*x = 0; 
+	public:
 		
-		else if (*x >= W)
-			*x = W - 1;
-			
+		Video(){}
+		void clamp(int * x, int * y);
+		void blank_frame() {  memset(frame, 0, sizeof(frame));}
+		void draw_rect(int x, int y, int w, int h, byte r, byte g, byte b);
+		void draw_frame(double t);
+		bool outside_frame(int * x, int * y);
+};
+
+using namespace std;
+
+void Video::clamp(int * x, int * y) {
+	
+	if (*x < 0)
+		*x = 0;
+			else if(*x >= W)
+				*x = W - 1;
+				
 	if (*y < 0)
 		*y = 0;
-		
-		else if (*y >= H)
-			*y = H - 1;
+			else if(*y >= H)
+				*y = H - 1;
 }
 
-bool outside_frame(int * x, int * y) {
-	return *x < 0 || *x >= W || *y < 0 || *y >= H;
-}
-
-// draws the rect
-void rect_figure(int x, int y, int h, int w, byte r, byte g, byte b) {
+void Video::draw_rect(int x, int y, int w, int h, byte r, byte g, byte b) {
 	
-	if (outside_frame(&x, &y))
+	if(outside_frame(&x, &y))
 		return;
 	int x0 = x;
 	int x1 = x + w;
 	int y0 = y;
 	int y1 = y + h;
-	
 	clamp(&x0, &y0);
 	clamp(&x1, &y1);
-	
 	for (int y = y0; y < y1; y++) {
 		for (int x = x0; x < x1; x++) {
-			frame[H][W][0] = r;
-			frame[H][W][1] = g;
-			frame[H][W][2] = b;
+			frame[y][x][0] = r;
+			frame[y][x][1] = b;
+			frame[y][x][2] = b;
 		}
-	}
+	}	
+}
+
+void Video::draw_frame(double t) {
+	blank_frame();
+	const double pps = 80;
+	draw_rect(100 + t * pps, 100 + t * pps, 2, 4, 0xff, 0xff, 0xff);
+}
+
+bool Video::outside_frame(int * x, int * y) {
+	
+	return *x < 0 || *x >= W || *y >= 0 || *y >= H;
+
 }
 
 int main(int argc, char * argv[]) {
 	// Construct the ffmpeg command to run.
-		const char * cmd = 
+	Video v;
+	const char * cmd = 
 		"ffmpeg              "
 		"-y                  "
 		"-hide_banner        "
@@ -97,16 +100,16 @@ int main(int argc, char * argv[]) {
 	}
 
 	// Write video frames into the pipe.
-	int num_frames = duration_per_second * frames_per_second;
+	int num_frames = duration_per_seconds * frame_per_seconds;
 	for (int i = 0; i < num_frames; ++i) {
-		double time_in_seconds = i / frames_per_second;
-		frame_drawn(time_in_seconds);
+		double time_in_seconds = i / frame_per_seconds;
+		v.draw_frame(time_in_seconds);
 		fwrite(frame, 3, W * H, pipe);
 	}
 
 	fflush(pipe);
 	pclose(pipe);
-	
+
 	cout << "num_frames: " << num_frames << endl;
 	cout << "Done." << endl;
 
